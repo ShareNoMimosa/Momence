d550bd0ab52c38183ae3fdf3a54e4a666edcb780
Mason Maeshiro <masonmaeshiro@gmail.com>
2023-03-21 15:45:15 -0700

    format exchange data

--- src/components/Exchange.tsx
+++ src/components/Exchange.tsx
@@ -12,19 +12,78 @@
 
 import React from 'react';
 
+type CurrencyData = {
+    country: string,
+    currency: string,
+    amount: number,
+    code: string,
+    rate: number
+};
+
+type FormattedExchangeData = { date: string, headers: Array<string>, currencies: Array<CurrencyData> };
+
+function formatExchangeData(data: string): FormattedExchangeData | null {
+    const rows = data.split('\n');
+    if (rows.length < 3) {
+        // invalid exchange data
+        return null;
+    }
+
+    const formattedData: FormattedExchangeData = {
+        date: '',
+        headers: [],
+        currencies: [],
+    };
+
+    const dateRow = rows.shift()?.split(' #')[0] || '';
+    formattedData.date = dateRow;
+
+    const headersRow = rows.shift()?.split('|') || [];
+    formattedData.headers = headersRow;
+
+    rows.map(row => {
+        const rowData = row.split('|');
+
+        if (rowData.length !== 5) {
+            return;
+        }
+
+        formattedData.currencies.push({
+            country: rowData[0],
+            currency: rowData[1],
+            amount: parseFloat(rowData[2]),
+            code: rowData[3],
+            rate: parseFloat(rowData[4])
+        });
+    });
+
+    return formattedData;
+}
+
 export default function Exchange() {
+    const [exchangeData, setExchangeData] = React.useState(null);
+
     React.useEffect(() => {
         const fetchData = async () => {
+            /**
+             * Actually will run into CORS error because cnb server returns
+             * 'Access-Control-Allow-Origin: apl.cnb.cz' header while we're
+             * running our react app on localhost:3000.
+             * 
+             * For this exercise bypassing CORS locally with a browser
+             * extension. A more practical solution would be standing up our
+             * own server to route request and return an
+             * 'Access-Control-Allow-Origin', '*' header instead. */
             const response = await fetch('https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt');
-            console.log('response', response);
 
             const responseText = await response.text();
 
-            console.log(responseText, response.body);
+            const formattedData = formatExchangeData(responseText);
+
+            console.log(responseText, formattedData);
         };
 
         fetchData();
-        console.log('test');
     }, []);
 
     return (
